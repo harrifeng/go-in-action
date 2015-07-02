d15ee914f59e9c701777c4323b9562c16011b642
Haoran Feng <haoran.feng@sap.com>
2015-07-02 02:06:29 -0700

    finish chapter 3

---
+++ introduction/ch3/main2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello" + "World")
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// 11											  //
+// 101											  //
+// HelloWorld									  //
+////////////////////////////////////////////////////
