75da234001ced44ea85c19861162f30e40f95b7e
Feng <haoran.feng@sap.com>
2015-11-22 23:49:49 -0800

    have a try

---
+++ try/gj01.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+)
+
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+}
+
+func main() {
+}
