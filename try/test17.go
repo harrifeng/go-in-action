ba0a48af5fa6bc3ec754b423ad49ab270547f15d
Harri Feng <harrifeng@gmail.com>
2016-02-27 14:04:36 +0800

    new comming

---
+++ try/test17.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+var period = flag.Duration("period", 1*time.Second, "sleep period")
+
+func main() {
+	flag.Parse()
+	fmt.Printf("Sleeping for %v...\n", *period)
+	time.Sleep(*period)
+	fmt.Println()
+
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// Sleeping for 1s...				              //
+////////////////////////////////////////////////////
