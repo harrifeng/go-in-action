4e89450d7ecb9084af2a8359b70a75560f4534ee
Haoran Feng <haoran.feng@sap.com>
2016-03-01 17:16:10 +0800

    wait for improve

--- regopl/1_4.go
+++ regopl/1_4.go
@@ -8,9 +8,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	filep := make(map[string]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, filep)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -18,22 +19,23 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, filep)
 			f.Close()
 		}
 	}
 
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%s %d\n", line, n)
+			fmt.Printf("str: '%s', times: %d, pos: %s\n", line, n, filep[line])
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, filep map[string]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		filep[input.Text()] += fmt.Sprintf("%v ", f.Name())
 	}
 }
 
