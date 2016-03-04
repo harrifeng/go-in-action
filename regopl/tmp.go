909da5271d5a48bc6f4dd4baebd60e9904c258df
Harri Feng <harrifeng@gmail.com>
2016-03-04 23:19:22 +0800

    ref is ok

--- regopl/tmp.go
+++ regopl/tmp.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+type Employee struct {
+	ID     int
+	Name   string
+	Salary int
+}
+
+var emp = []Employee{
+	{1, "Adaa", 2500},
+	{2, "Bell", 3500},
+}
+
+func EmployeeByID(id int) *Employee {
+	if id == 1 {
+		return &emp[0]
+	}
+	if id == 2 {
+		return &emp[1]
+	}
+	return nil
+}
+
 func main() {
-	fmt.Printf("%v", 123.89)
+	EmployeeByID(1).Salary = 24
+	fmt.Println(EmployeeByID(1).Salary)
 }
