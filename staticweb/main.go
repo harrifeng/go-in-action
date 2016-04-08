03022cf685b43fb3814254e3924be80933d22965
Haoran Feng <haoran.feng@sap.com>
2016-04-08 16:42:44 +0800

    package http function actually work in this way!! func ListenAndServe(addr string, handler Handler) error { 	server := &Server{Addr: addr, Handler: handler} 	return server.ListenAndServe() }

--- staticweb/main.go
+++ staticweb/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func messageHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,8 +16,14 @@ func main() {
 
 	http.HandleFunc("/welcome", messageHandler)
 
+	server := &http.Server{
+		Addr:           ":8080",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
 	log.Println("Listening...")
 
-	http.ListenAndServe(":8080", nil)
+	server.ListenAndServe()
 	os.Exit(0)
 }
