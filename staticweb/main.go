fd856535472c5d7df2b4dcf07e669122b0b04fca
Haoran Feng <haoran.feng@sap.com>
2016-04-08 14:54:40 +0800

    create a struct to have method ServeHTTP

--- staticweb/main.go
+++ staticweb/main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
+type messageHandler struct {
+	message string
+}
+
+func (m *messageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, m.message)
+}
+
 func main() {
 	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir("public"))
-	mux.Handle("/", fs)
+
+	mh1 := &messageHandler{"Welcome to Go Web Dev"}
+	mux.Handle("/welcome", mh1)
+
+	mh2 := &messageHandler{"net/http is awesome"}
+	mux.Handle("/message", mh2)
+
+	log.Println("Listening...")
+
 	http.ListenAndServe(":8080", mux)
-	fmt.Println()
 	os.Exit(0)
 }
